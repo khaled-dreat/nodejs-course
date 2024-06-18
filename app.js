fcd74c0fad57247da76a663c3beabec9fca5b1e9
khaled-dreat <dreatkhaled@gmail.com>
2024-06-18 12:24:12 +0300

    Working with Handlebars

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 const path = require('path');
-
+const expressHbs = require('express-Handlebars');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', express());
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
