2fb89de1119bcbfcf1bef5ff7225c7d0fee16992
khaled-dreat <dreatkhaled@gmail.com>
2024-06-18 12:15:00 +0300

    Finishing the Pug Template

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-const express = require('express');
 const path = require('path');
-const bodyparser = require('body-parser');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", "views");
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
-const adminData = require("./route/admin");
-const shopRoutes = require("./route/shop");
-app.use(bodyparser.urlencoded({ extendeds: false }));
-app.use(express.static(path.join(__dirname, "public")))
-app.use("/admin", adminData.router);
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
+
 app.listen(3000);
